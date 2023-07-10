c45ccb941bd7d5ea1b4835bc1c9757203d648c40
Ali Furkan Terzi <alifurkanterzi16@gmail.com>
2023-07-10 21:04:48 +0300

    Add project files.

---
+++ 01-Factorial_Method/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Please enter a number : ");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("factorial = {0}", factorial(num));
+
+
+int factorial(int num)
+{
+    if (num == 1)
+    {
+        return 1;
+    }
+    else if (num == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return num * factorial(num - 1);
+    }
+}
